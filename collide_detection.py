984da567df9898505bc3dc868132bd36f1d010bf
MeteorYu <1040178563@qq.com>
2023-04-13 22:21:20 +0800

    aabb算法：aabb构建中，换了一个思路构建包围盒，不通过对角点构建box，而是中心点！！之前没想到真是纯纯笨蛋！！

--- collide_detection.py
+++ collide_detection.py
@@ -10,6 +10,7 @@ class aabb_obj:
         self.tree_size = int((8 ** self.layer_num + 13) / 7)
         self.aabb_root = ti.Vector.field(3, ti.f32, shape=8)
         self.aabb_tree = ti.Vector.field(3, ti.f32, shape=self.tree_size)
+        self.box_edge_len = ti.Vector.field(3, ti.f32, shape=1)
 
     def get_root(self):
         x_np = self.verts.x.to_numpy()
@@ -17,21 +18,10 @@ class aabb_obj:
         self.aabb_tree[1] = x_np.max(0)
 
     @ti.kernel
-    def get_aabb_root(self):
-        self.aabb_root[0] = self.aabb_tree[0]
-        self.aabb_root[1] = [self.aabb_tree[0].x, self.aabb_tree[0].y, self.aabb_tree[1].z]
-        self.aabb_root[2] = [self.aabb_tree[0].x, self.aabb_tree[1].y, self.aabb_tree[0].z]
-        self.aabb_root[3] = [self.aabb_tree[0].x, self.aabb_tree[1].y, self.aabb_tree[1].z]
-        self.aabb_root[4] = [self.aabb_tree[1].x, self.aabb_tree[0].y, self.aabb_tree[0].z]
-        self.aabb_root[5] = [self.aabb_tree[1].x, self.aabb_tree[0].y, self.aabb_tree[1].z]
-        self.aabb_root[6] = [self.aabb_tree[1].x, self.aabb_tree[1].y, self.aabb_tree[0].z]
-        self.aabb_root[7] = self.aabb_tree[1]
-
-    @ti.kernel
     def get_aabb_tree(self):
-        base_z = (self.aabb_root[1] - self.aabb_tree[0]).z
-        base_x = (self.aabb_root[4] - self.aabb_tree[0]).x
-        base_y = (self.aabb_root[2] - self.aabb_tree[0]).y
+        self.box_edge_len[0].x = self.aabb_tree[1].x - self.aabb_tree[0].x
+        self.box_edge_len[0].y = self.aabb_tree[1].y - self.aabb_tree[0].y
+        self.box_edge_len[0].z = self.aabb_tree[1].z - self.aabb_tree[0].z
         layer_node_num = 1
         for layer in ti.static(range(1, self.layer_num + 1)):
             m = 2 ** layer
@@ -41,12 +31,10 @@ class aabb_obj:
                 n2 = 2 * int(k2 / (m / 2) + 1) - 1
                 n3 = 2 * (node % (m / 2) + 1) - 1
                 self.aabb_tree[layer_node_num + node] = \
-                    self.aabb_tree[0] + [n1 / m * base_x, n2 / m * base_y, n3 / m * base_z]
+                    self.aabb_tree[0] + \
+                    [n1 / m * self.box_edge_len[0].x, n2 / m * self.box_edge_len[0].y, n3 / m * self.box_edge_len[0].z]
             layer_node_num += (2 ** (3 * (layer - 1)))
 
     def run(self):
         self.get_root()
-        self.get_aabb_root()
         self.get_aabb_tree()
-
-
--- main.py
+++ main.py
@@ -4,9 +4,36 @@ import collide_detection as cd
 import haptics as ha
 import pyhaptics as ph
 
+def ggui_init():
+    window = ti.ui.Window("FEM", (768, 768), vsync=False)
+    canvas = window.get_canvas()
+    canvas.set_background_color(color=(1, 1, 1))
+    scene = ti.ui.Scene()
+    camera = ti.ui.Camera()
+    camera.up(0, 1, 0)
+    camera.position(0.0, 0.0, 0.35)
+    camera.lookat(0.0, 0.0, 0.0)
+    camera.fov(75)
+    return window, canvas, scene, camera
+
+def ggui_run(window, canvas, scene, camera):
+    camera.track_user_inputs(window, 0.008, hold_key=ti.ui.RMB)
+    scene.set_camera(camera)
+    scene.ambient_light((0.5, 0.5, 0.5))
+    scene.point_light(camera.curr_position, (0.7, 0.7, 0.7))
+    scene.mesh(model.mesh.verts.x, model.indices, color=(1.0, 0.3, 0.3))
+    scene.mesh(equipment_model.mesh.verts.x, equipment_model.indices, color=(0.7, 0.7, 0.7))
+    scene.particles(bvt_obj.aabb_tree, 0.002, (0.9, 0.9, 0.9), index_offset=2, index_count=bvt_obj.tree_size - 2)
+    scene.particles(bvt_equipment.aabb_tree, 0.002, (0.9, 0.9, 0.9),
+                    index_offset=2, index_count=bvt_equipment.tree_size - 2)
+    canvas.scene(scene)
+    window.show()
+
+
 if __name__ == '__main__':
     ti.init(arch=ti.cuda)
-    ph.init()
+    # ph.init()
+    window, canvas, scene, camera = ggui_init()
 
     obj = "model/liver/liver0.node"
     equipment = "model/equipment/Scissors.stl"
@@ -19,15 +46,7 @@ if __name__ == '__main__':
 
     hap = ha.haptices(equipment_model.mesh.verts)
 
-    window = ti.ui.Window("FEM", (768, 768), vsync=False)
-    canvas = window.get_canvas()
-    canvas.set_background_color(color=(1, 1, 1))
-    scene = ti.ui.Scene()
-    camera = ti.ui.Camera()
-    camera.up(0, 1, 0)
-    camera.position(0.0, 0.0, 0.35)
-    camera.lookat(0.0, 0.0, 0.0)
-    camera.fov(75)
+
 
     while window.running:
         if window.is_pressed('r'):
@@ -42,18 +61,4 @@ if __name__ == '__main__':
 
         hap.run()
 
-        camera.track_user_inputs(window, 0.008, hold_key=ti.ui.RMB)
-        scene.set_camera(camera)
-        scene.ambient_light((0.5, 0.5, 0.5))
-        scene.point_light(camera.curr_position, (0.7, 0.7, 0.7))
-        scene.mesh(model.mesh.verts.x, model.indices, color=(1.0, 0.3, 0.3))
-        scene.mesh(equipment_model.mesh.verts.x, equipment_model.indices, color=(0.7, 0.7, 0.7))
-        scene.particles(bvt_obj.aabb_root, 0.002, (0.2, 0.2, 0.2))
-        scene.particles(bvt_obj.aabb_tree, 0.002, (0.9, 0.9, 0.9), index_offset=2, index_count=bvt_obj.tree_size - 2)
-        scene.particles(bvt_equipment.aabb_root, 0.002, (0.2, 0.2, 0.2))
-        scene.particles(bvt_equipment.aabb_tree, 0.002, (0.9, 0.9, 0.9),
-                        index_offset=2, index_count=bvt_equipment.tree_size - 2)
-        canvas.scene(scene)
-        window.show()
-
-    # ti.profiler.print_kernel_profiler_info()
+        ggui_run(window, canvas, scene, camera)
